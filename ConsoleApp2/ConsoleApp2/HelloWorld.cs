e26c6163043acd1b0e89be645f08450e3a593961
Manuela Larrea <mlarrea286@soyudemedellin.edu.co>
2022-11-07 00:33:43 -0500

    Hello world

---
+++ ConsoleApp2/ConsoleApp2/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp2
+{
+    public static class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(ReturnHelloWorld());
+        }
+        public static string ReturnHelloWorld()
+        {
+            return ("Hello, world!!");
+        }
+
+    }
+}
